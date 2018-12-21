d435a596caf37c8203bd9e0eb7913fde3acd7547
FostByte01 <39464343+FostByte01@users.noreply.github.com>
2018-12-21 15:54:39 -0600

    Takes CLI Args

--- src/euclid.cpp
+++ src/euclid.cpp
@@ -1,14 +1,12 @@
 #include<iostream>
 
 int euclidAlgo(const int& lhs, const int& rhs);
+int parseToInt(const char* args[], const int& argNum);
 
 int main(){
 	int lhs, rhs;
-	std::cout << "Enter two numbers to run Euclid's algorithim" << std::endl;
-	std::cout << "Number A: ";
-	std::cin >> lhs;
-	std::cout << "Number B: ";
-	std::cin >> rhs;
+	lhs = parseToInt(argv, 1);
+	rhs = parseToInt(argv, 2);
 	std::cout << euclidAlgo(lhs, rhs) <<std::endl;
 	return 0;
 }
@@ -20,5 +18,20 @@ int euclidAlgo(const int& lhs, const int& rhs){
 	}else{
 		std::cout << "---------" << std::endl << "Number A: " << lhs << std::endl << "Number B: " << rhs << std::endl;
 		return euclidAlgo(rhs % lhs, lhs);
+	}
+}
+
+
+int parseToInt(const char* args[], const int& argNum){
+	/* This function will attempt to convert
+	the first command line argument to an integer,
+	if it fails, the program will exit, if not,
+	it will return the first argument as an integer */
+	try{
+		int choice = std::stoi(args[argNum]);
+		return choice;
+	}catch(const std::exception& e){
+		std::cout << "Argument could not be parsed, make sure you entered and integer!" << std::endl;
+		exit(1);
 	}
 }
